a36eb4aa9145fa1e0b4362a1abf58c8929f03ded
RomSboy <Roman.sboyev@gmail.com>
2019-02-26 22:25:21 +0200

    fixed models, added migrations, made 'results' template and all are working

--- first_Django_site/polls/views.py
+++ first_Django_site/polls/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render, get_object_or_404
-from django,http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 def detail(request, question_id):
-    question = get_object_or_404(Question, question_id)
+    question = get_object_or_404(Question, pk=question_id)   
     return render(request, 'polls/detail.html', {'question': question})
 
 
@@ -14,20 +14,28 @@ def index(request):
     context={'latest_question_list': latest_qustion_list,}
     return render(request, 'polls/index.html', context)
 
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        selected_choice = question.choice_set.get(pk=request.POST['choice']) #request.POST return the ID by key 'choice', as a string
     except (KeyError, Choice.DoesNotExist):
         #Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
             'question': question,
-            'error_message': "You didnot select a choice"
+            'error_message': "You didnot select a choice",
         })
     else:
         selected_choice.votes += 1
         selected_choice.save()
-         # Always return an HttpResponseRedirect after successfully dealing
+        # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-    return HttpResponseRedirect(reverse('polls:results', args=(question_id)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))  #comma is very important after question_id
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+        
